c74b23b5212c4e665623e4c0d4258b815f20e49f
jantar2b <jtsuchanek@gmail.com>
2013-10-28 15:42:04 -0500

    Random changes

--- db/migrate/20131015010219_create_jobs.rb
+++ db/migrate/20131015010219_create_jobs.rb
@@ -1,6 +1,8 @@
 class CreateJobs < ActiveRecord::Migration
   def change
     create_table :jobs do |t|
+      t.string :title
+      t.string :description
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,28 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131014222108) do
+ActiveRecord::Schema.define(:version => 20131015165104) do
+
+  create_table "jobs", :force => true do |t|
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+    t.string   "title"
+    t.string   "description"
+  end
+
+  create_table "microposts", :force => true do |t|
+    t.string   "content"
+    t.integer  "user_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "projects", :force => true do |t|
+    t.string   "name"
+    t.string   "description"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "name"
